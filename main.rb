9ad9e42eba956d43978c22f739bda4785becd0df
michimichi2021 <michikaho1009@gmail.com>
2021-12-14 22:46:00 +0900

    3-4終了

--- main.rb
+++ main.rb
@@ -17,23 +17,55 @@ class Brave
   def attack(monster)
    puts "#{@name}の攻撃"
 
+   #decision_attack_typeメソッドの呼び出し
+   attack_type = decision_attack_type
+
+   #calculate_damageメソッドの呼び出し
+   damage = calculate_damage(target: monster,attack_type: attack_type)
+
+   #ダメージをHPに反映させる
+   cause_damage(target: monster,damage: damage)
+
+
+   puts "#{monster.name}の残りHPは#{monster.hp}だ"
+  end
+
+  private
+
+  def decision_attack_type
    #0~3の間でランダムに数字が変わる
    attack_num = rand(4)
 
    #4分の1の確率でcalculate_special_attackを実行
    if attack_num == 0
     puts "必殺攻撃"
-    damage = calculate_special_attack - monster.defense
+    "special_attack"
    else
     puts "通常攻撃"
-    damage = @offense - monster.defense
+    "normal_attack"
    end
+  end
 
-   #自己代入:monster.hpからdamageを引いた値をmonster.hpに代入
-   monster.hp -= damage
+  #ダメージの計算メソッド
+  def calculate_damage(**params)
+    target = params[:target]
+    attack_type = params[:attack_type]
 
-   puts "#{monster.name}は#{damage}のダメージを受けた"
-   puts "#{monster.name}の残りHPは#{monster.hp}だ"
+    if attack_type == "special_attack"
+      calculate_special_attack - target.defense
+    else
+      @offense - target.defense
+    end
+
+  end
+
+  #HPにダメージを反映させる
+  def cause_damage(**params)
+   damage = params[:damage]
+   target = params[:target]
+
+   target.hp -= damage
+   puts "#{target.name}は#{damage}のダメージを受けた"
   end
 
   def calculate_special_attack
